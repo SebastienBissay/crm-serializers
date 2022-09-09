16c777b906ffd7417f9d70465df0df20d8dc66ec
Sébastien BISSAY <seb.bissay@gmail.com>
2022-09-09 14:00:14 +0200

    Correctif annotations manquantes

--- src/main/java/groupe3/crm/service/implementation/UserServiceImplementation.java
+++ src/main/java/groupe3/crm/service/implementation/UserServiceImplementation.java
@@ -2,11 +2,15 @@ package groupe3.crm.service.implementation;
 
 import groupe3.crm.model.User;
 import groupe3.crm.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Sebastien Bissay
  */
+@Service
+@Transactional
 public class UserServiceImplementation extends AbstractServiceImplementation<User, UserRepository> {
 
     public UserServiceImplementation() {
